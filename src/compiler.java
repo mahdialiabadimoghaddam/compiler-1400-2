7d0c0f7e7d0f7dbb326874c0b36c223b530c28ea
mahdialiabadimoghaddam <aiabadimahdi@gmaile.com>
2022-05-04 17:43:14 +0430

    setup compiler.java

---
+++ src/compiler.java
@@ -0,0 +1,26 @@
+import gen.jythonLexer;
+import gen.jythonListener;
+import gen.jythonParser;
+import org.antlr.v4.runtime.tree.ParseTree;
+import org.antlr.v4.runtime.CharStream;
+import org.antlr.v4.runtime.CharStreams;
+import org.antlr.v4.runtime.CommonTokenStream;
+import org.antlr.v4.runtime.TokenStream;
+import org.antlr.v4.runtime.tree.ParseTreeWalker;
+
+import java.io.IOException;
+
+public class Compiler {
+    public static void main(String[] args) throws IOException {
+        CharStream stream = CharStreams.fromFileName("./input.txt");
+        jythonLexer lexer = new jythonLexer(stream);
+        TokenStream tokens = new CommonTokenStream(lexer);
+        jythonParser parser = new jythonParser(tokens);
+        parser.setBuildParseTree(true);
+        ParseTree tree = parser.program();
+        ParseTreeWalker walker = new ParseTreeWalker();
+        jythonListener listener = new ProgramPrinter();
+
+        walker.walk(listener, tree);
+    }
+}
